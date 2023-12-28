d6593ef7d9977098ff84e9d941ae02599701758a
Petar Stojkovski <petars089@gmail.com>
2023-12-28 14:37:26 +0100

    feat(client): Added shop component and restructure files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient)
-  {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => {
-        console.log("request completed");
-      }
-    });
-  }
+  ngOnInit(): void {}
 }
