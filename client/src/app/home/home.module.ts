85601aaf0df3511f5f93c2c186324861b2324e8b
Petar Stojkovski <petars089@gmail.com>
2023-12-31 12:53:05 +0100

    feat(client): Added routing functionality

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
-
-
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class HomeModule { }
