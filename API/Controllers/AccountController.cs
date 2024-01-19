6cb81dde3ef6b3967336539a58eabdeed415aafb
Petar Stojkovski <petars089@gmail.com>
2024-01-19 14:15:26 +0100

    feat(api): Added api endpoints for getting and updating user address

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
 using System.Net;
 using API.Dtos;
 using API.Errors;
+using API.Extenstions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +15,64 @@ public class AccountController : BaseApiController
     private readonly UserManager<AppUser> _userManager;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
     public AccountController(UserManager<AppUser> userManager,
         SignInManager<AppUser> signInManager,
-        ITokenService tokenService)
+        ITokenService tokenService,
+        IMapper mapper)
     {
         _tokenService = tokenService;
+        _mapper = mapper;
         _signInManager = signInManager;
         _userManager = userManager;
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var user = await _userManager.FindByEmailFromClaimsPrincipal(User);
+
+        return new UserDto
+        {
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user),
+            DisplayName = user.DisplayName
+        };
+    }
+
+    [HttpGet("email-exists")]
+    public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
+    {
+        return await _userManager.FindByEmailAsync(email) != null;
+    }
+
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<AddressDto>> GetUserAddress()
+    {
+        var user = await _userManager.FindByEmailFromClaimsPrincipal(User, includeAddress: true);
+
+        return _mapper.Map<Address, AddressDto>(user.Address);
+    }
+
+    [Authorize]
+    [HttpPut("address")]
+    public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+    {
+        var user = await _userManager.FindByEmailFromClaimsPrincipal(User, includeAddress: true);
+
+        user.Address = _mapper.Map<AddressDto, Address>(address);
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if(!result.Succeeded) 
+        {
+            return BadRequest("Problem with updating user.");
+        }
+
+        return Ok(_mapper.Map<Address, AddressDto>(user.Address));
     }
 
     [HttpPost("login")]
---
+++ API/Extenstions/UserManagerExtenstions.cs
@@ -0,0 +1,22 @@
+using System.Security.Claims;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extenstions;
+
+public static class UserManagerExtenstions
+{
+    public static async Task<AppUser> FindByEmailFromClaimsPrincipal(this UserManager<AppUser> userManager,
+    ClaimsPrincipal user, bool includeAddress = false)
+    {
+        var query = userManager.Users;
+
+        if (includeAddress)
+        {
+            query = query.Include(x => x.Address);
+        }
+
+        return await query.SingleOrDefaultAsync(x => x.Email == user.FindFirstValue(ClaimTypes.Email));
+    }
+}
