f1321c20c24a4739b9b61f83dfcb45172c2d69ed
Petar Stojkovski <petars089@gmail.com>
2024-01-20 19:34:02 +0100

    feat(client): Added loading current user functionality and displaying it on the nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,31 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(): void {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return;
+    }
+
+    this.accountService.loadCurrentUser(token).subscribe();
   }
 
   private loadBasket(): void {
     const basketId = localStorage.getItem('basket_id');
-
-    if (basketId) {
-      this.basketService.getBasket(basketId);
+    if (!basketId) {
+      return;
     }
+
+    this.basketService.getBasket(basketId);
   }
 }
