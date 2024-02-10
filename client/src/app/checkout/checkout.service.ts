4a47ca1e2779b38d51f30d3a9c1a16273cd35ea2
Petar Stojkovski <petars089@gmail.com>
2024-02-10 16:38:20 +0100

    feat(client): Implemented initial checkout-delivery component design and functionality

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import { Observable, map } from 'rxjs';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDeliveryMethods(): Observable<DeliveryMethod[]> {
+    return this.http
+      .get<DeliveryMethod[]>(this.baseUrl + 'orders/delivery-methods')
+      .pipe(
+        map((dm) => {
+          return dm.sort((first, next) => next.price - first.price);
+        })
+      );
+  }
 }
