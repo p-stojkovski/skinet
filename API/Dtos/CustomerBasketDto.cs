014d74d4912af4d4cbc7af0a37832caf62f08cad
Petar Stojkovski <petars089@gmail.com>
2024-02-24 11:57:38 +0100

    feat(api): Extended saving and retriving basket with delivery method Id

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -6,4 +6,5 @@ public class CustomerBasketDto
     [Required]
     public string Id { get; set; }
     public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+    public int DeliveryMethodId { get; set; }
 }
